b9b04a8a93ad1e5a7d7bc7e7e1431281ad684a36
Chinedu Chaz <ccognate@gmail.com>
2022-10-13 07:04:18 -0700

    Betty missing blank line after declaration fixed

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,6 +14,7 @@ int main(void)
 	long int li;
 	long long int  lli;
 	float fd;
+
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(li));
