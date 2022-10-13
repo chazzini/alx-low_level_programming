5b628e4b436526125a9fdc53b479ae855edd8998
Chinedu Chaz <ccognate@gmail.com>
2022-10-13 06:00:03 -0700

    Adding comment s to file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+/**
+ * Using  puts() functions
+ * to output string to the 
+ * *standard output
+ * 
+ */
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle");
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+ * Using printf to output a string to 
+ * standard output
+ */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
