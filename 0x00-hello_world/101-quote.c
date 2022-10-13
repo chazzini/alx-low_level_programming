a83eae88cc074e5240cb3fb4b3c6cc2efec552c8
Chinedu Chaz <ccognate@gmail.com>
2022-10-13 07:56:01 -0700

    Outputing using standard output

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main Program entry point
+ * output to standard error
+ * wthout puts() or printf
+ * Return : Always 1 (Success)
+ */
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\"
+			- Dora Korpar, 2015-10-19\n");
+
+	return (1);
+}
