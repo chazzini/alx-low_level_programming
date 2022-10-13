29467738e53a8f610c312ace0913cc700cda79d5
Chinedu Chaz <ccognate@gmail.com>
2022-10-13 08:16:44 -0700

    Final correction

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,9 +9,9 @@
 
 int main(void)
 {
-	char[] s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char s[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	fprintf(stderr, s);
+	fprintf(stderr, "%s", s);
 
 	return (1);
 }
