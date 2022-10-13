d75887bb26694d4cd8bb3157def9e6a7d5cb6f14
Chinedu Chaz <ccognate@gmail.com>
2022-10-13 06:59:41 -0700

    Output various size of different intrinsic type

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Printing out the size of various intrinsic type to
+ * standard output
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	char c;
+	long int li;
+	long long int  lli;
+	float fd;
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(li));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lli));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(fd));
+
+	return (0);
+}
