c4b84ceaad7b8e21b73065cfce2696ceaba2cf34
Chinedu Chaz <ccognate@gmail.com>
2022-10-14 09:47:50 -0700

    Exculde q and e in the out using putchar for the alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/*
+ * You can only use the putchar
+ * function (every other function (printf, puts, etc…) is forbidden)
+ * All your code should be in the main function
+ * You can only use putchar three times in your code
+ * Skip the print of  q and e
+ */
+
+/**
+ * main - Entry point of the program
+ *
+ *Return: 0 Always (Success)
+ */
+int main(void)
+{
+	int j = 0;
+	char s[] = "abcdefghijklmnopqrstuvwxyz";
+
+	while (s[j])
+	{
+		if (s[j] == 'q' || s[j] == 'e')
+		{
+			j++;
+			continue;
+		} else
+		{
+			putchar(s[j]);
+		}
+
+		j++;
+	}
+	putchar('\n');
+	return (0);
+}
