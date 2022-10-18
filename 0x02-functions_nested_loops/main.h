9b3b6d9909c3a8163f69b797ff9f225b5bf6e5d7
Chinedu Chaz <ccognate@gmail.com>
2022-10-18 13:29:18 -0700

    Program to check if character is lower case

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,7 +4,7 @@
 void print_alphabet_x10(void);
 void print_alphabet(void);
 int _putchar(char c);
-int _islower(int c);
+int _islower(char c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
