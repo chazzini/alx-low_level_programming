8e1b5467318b745efacfd2a6c676efaa7cad3399
Chinedu Chaz <ccognate@gmail.com>
2022-10-20 08:24:59 -0700

    Debugged print remaining days function modified corrected

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -26,14 +26,16 @@ void print_remaining_days(int month, int day, int year)
 	}
 
 	if (isleap == 1)
+	{
+
 
 		if (month > 2 && day >= 60)
 		{
 			day++;
 		}
 
-			printf("Day of the year: %d\n", day);
-			printf("Remaining days: %d\n", 366 - day);
+		printf("Day of the year: %d\n", day);
+		printf("Remaining days: %d\n", 366 - day);
 	}
 	else
 	{
