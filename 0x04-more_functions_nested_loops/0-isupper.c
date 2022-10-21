f856d0ae51ff02928a2e22fe0a0a03264eefcfdb
Chinedu Chaz <ccognate@gmail.com>
2022-10-20 23:43:52 -0700

    Isupper function

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+/**
+ * check if character is uppercase
+ * @c: character to check
+ * Return: 1 is uppercase and 0 if not uppercase
+ */
+int _isupper(char c)
+{
+	int s = (int) c; /*cast the c to int*/
+
+	if (s >= 65 && s <= 90)
+		return 1;
+	return 0;
+}
