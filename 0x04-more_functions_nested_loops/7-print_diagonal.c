6a2d50444b572c305897a7724a29f5a551712793
Chinedu Chaz <ccognate@gmail.com>
2022-10-21 02:00:15 -0700

    Diagonal printing

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print line
+ * @s: count for line
+ * Return: void
+ */
+void print_diagonal(int s)
+{
+	int i, j;
+
+
+	for (i = 0; i <= s; i++)
+	{
+		if (s == 0)
+			break;
+		j = i;
+
+		while (j--)
+		{
+			_putchar(' ');
+		}
+		_putchar(92);
+		_putchar('\n');
+
+	}
+
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -9,5 +9,6 @@ void print_numbers(void);
 void print_most_numbers(void);
 void print_line(int s);
 int mul(int a, int b);
+void print_diagonal(int s);
 void more_numbers(void);
 #endif/*MAIN_H*/
