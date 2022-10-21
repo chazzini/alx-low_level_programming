1f1e86f2f9ac91a438336ad2e46978f0bb0094a0
Chinedu Chaz <ccognate@gmail.com>
2022-10-21 00:11:33 -0700

    Check if number is between  0 and 9

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+/**
+ * _isdigit - check if number is 1 to 9
+ * @s: number to check
+ * Return: 1 is uppercase and 0 if not uppercase
+ */
+int _isdigit(int s)
+{
+
+	if (s >= 48 && s <= 57)
+		return (1);
+	return (0);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,5 +3,5 @@
 #include <stdio.h>
 
 int _isupper(char c);
-
+int _isdigit(int c);
 #endif/*MAIN_H*/
