0de3b8acad3bedd271c3333f83d7fb75a0f01b8a
Chinedu Chaz <ccognate@gmail.com>
2022-10-21 01:25:11 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - print line
+ * @s: count for line
+ * Return: void
+ */
+void print_line(int s)
+{
+	int i;
+
+
+	for (i = 0; i <= s; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -7,6 +7,7 @@ int _isdigit(int c);
 int _putchar(char c);
 void print_numbers(void);
 void print_most_numbers(void);
+void print_line(int s);
 int mul(int a, int b);
 void more_numbers(void);
 #endif/*MAIN_H*/
