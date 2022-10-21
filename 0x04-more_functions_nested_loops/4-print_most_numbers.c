efd2de7b2469d55c9478dfa252a82e1b5fc0b469
Chinedu Chaz <ccognate@gmail.com>
2022-10-21 00:39:54 -0700

    Print must number

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print 0 to  9
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i == 4 || i == 2)
+			continue;
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+
+}
