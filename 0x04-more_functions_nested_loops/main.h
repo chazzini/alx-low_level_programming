59558c213fdfcafd90e6fb207236de722a4cb784
Chinedu Chaz <ccognate@gmail.com>
2022-10-21 02:09:11 -0700

    print square

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -9,6 +9,7 @@ void print_numbers(void);
 void print_most_numbers(void);
 void print_line(int s);
 int mul(int a, int b);
+void print_square(int s);
 void print_diagonal(int s);
 void more_numbers(void);
 #endif/*MAIN_H*/
