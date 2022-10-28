8a39298d7b398d273ff9c36a5b8b848672965ccb
Chinedu Chaz <ccognate@gmail.com>
2022-10-27 20:26:39 -0700

    fixed reverse array count

--- 0x06-pointers_arrays_strings/4-rec_array.c
+++ 0x06-pointers_arrays_strings/4-rec_array.c
@@ -1,23 +1,27 @@
 #include "main.h"
 /**
- * reverse_array - reverses the content of an array
+ * reverse_arraay - reverses the content of an array
  * of integers.
  * @a: array.
  * @n: number of elements of the array.
  * Return: no return.
  */
+#include "holberton.h"
 
 void reverse_array(int *a, int n)
 {
-	int i, j, temp;
+	int i, last;
 
-	for (i = 0; i < n - 1; i++)
+	last = n - 1;
+	for (i = 0; i < n / 2; i++)
 	{
-		for (j = i + 1; j > 0; j--)
-		{
-			temp = *(a + j);
-			*(a + j) = *(a + (j - 1));
-			*(a + (j - 1)) = temp;
-		}
+		int start, end;
+
+		start = a[i];
+		end = a[last];
+		a[i] = end;
+		a[last] = start;
+		last--;
 	}
 }
+
