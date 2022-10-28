dc7c3dadd8c505e8ec21692def00fed854cd1931
Chinedu Chaz <ccognate@gmail.com>
2022-10-27 20:35:46 -0700

    Fix betty doc

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,6 +1,7 @@
 #include "main.h"
+
 /**
- * reverse_arraay - reverses the content of an array
+ * reverse_array - reverses the content of an array
  * of integers.
  * @a: array.
  * @n: number of elements of the array.
