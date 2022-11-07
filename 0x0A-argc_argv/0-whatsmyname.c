a57ac9ecabbe3c652707a2d80c44854248cdf2a9
Chinedu Chaz <ccognate@gmail.com>
2022-11-06 20:59:44 -0800

    fix betty formatting

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,18 +3,19 @@
 
 /**
  * main - entry point of the program
+ *
  * @argc : argument count
  * @argv : argument value
- * return 0 success
+ * Return: 0 success
  */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 
 	int i;
 
 	for (i = 0; i < argc; i++)
-		printf("%s\n",argv[i]);
+		printf("%s\n", argv[i]);
 
-	return 0;
+	return (0);
 
 }
