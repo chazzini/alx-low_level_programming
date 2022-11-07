a0eb5624a314d42fd4f1e35b6c2d4858f6dc8be4
Chinedu Chaz <ccognate@gmail.com>
2022-11-06 21:11:12 -0800

    Write a program that prints the number of argument
    Your program should print a number, followed by a new line

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point of the program
+ *
+ * @argc : argument count
+ * @argv : argument value
+ * Return: 0 success
+ */
+int main(int argc, char *argv[] __attribute((unused)))
+{
+	printf("%d\n", argc -1);
+	return (0);
+}
